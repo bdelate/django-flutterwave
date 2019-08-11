1553a359f39aa6b48d214d50eb2ca860b7e370bf
bdelate <brendon.delate@gmail.com>
2019-08-11 07:09:57 +0200

    Refactor models to remove plan and onceoff models and instead only have paymentmethod model and transaction model

--- djangorave/models.py
+++ djangorave/models.py
@@ -1,6 +1,7 @@
 # stdlib import
 
 # django imports
+from django.conf import settings
 from django.db import models
 
 # 3rd party imports
@@ -8,41 +9,40 @@ from django.db import models
 # project imports
 
 
-class PaymentBaseModel(models.Model):
-    """Represents the base data required for either a Plan or OnceOff payment
-    method"""
+class PaymentMethodModel(models.Model):
+    """Represents either a Plan or OnceOff payment method"""
 
     description = models.CharField(max_length=50, unique=True)
     amount = models.DecimalField(decimal_places=2, max_digits=9)
+    payment_plan = models.PositiveIntegerField(unique=True, blank=True, null=True)
     currency = models.CharField(max_length=3, default="USD")
     custom_logo = models.URLField(max_length=500, blank=True, null=True)
     custom_title = models.CharField(max_length=200, blank=True, null=True)
     pay_button_text = models.CharField(max_length=100, default="Sign Up")
     payment_options = models.CharField(max_length=100, default="card")
 
-    class Meta:
-        abstract = True
-
     def __str__(self):
         return self.description
 
 
-class PlanModel(PaymentBaseModel):
-    """Represents a recurring payment plan
+class TransactionModel(models.Model):
+    """Represents a transaction for a specific payment method and user"""
 
-    payment_plan: Rave plan ID
-    """
+    payment_method = models.ForeignKey(
+        to=PaymentMethodModel, related_name="transactions", on_delete=models.CASCADE
+    )
+    user = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL,
+        related_name="transactions",
+        on_delete=models.CASCADE,
+    )
+    created_datetime = models.DateTimeField(auto_now_add=True)
+    reference = models.CharField(max_length=200, unique=True)
+    flutterwave_reference = models.CharField(max_length=200, unique=True)
+    order_reference = models.CharField(max_length=200, unique=True)
+    amount = models.DecimalField(decimal_places=2, max_digits=9)
+    charged_amount = models.DecimalField(decimal_places=2, max_digits=9)
+    status = models.CharField(max_length=50)
 
-    payment_plan = models.PositiveIntegerField(unique=True)
-
-    class Meta(PaymentBaseModel.Meta):
-        verbose_name = "Plan"
-        verbose_name_plural = "Plans"
-
-
-class OnceOffModel(PaymentBaseModel):
-    """Represents a once off payment"""
-
-    class Meta(PaymentBaseModel.Meta):
-        verbose_name = "Once Off Payment Option"
-        verbose_name_plural = "Once Off Payment Options"
+    def __str__(self):
+        return self.reference
