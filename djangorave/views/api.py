2fb26838a249db366b0b5cae6fd9c9f2fc01a75e
bdelate <brendon.delate@gmail.com>
2019-08-12 06:56:56 +0200

    Implement perform_create on TransactionApiView to add payment_method and user to new instance

--- djangorave/views/api.py
+++ djangorave/views/api.py
@@ -1,23 +1,30 @@
 # stdlib imports
 
 # django imports
+from django.contrib.auth import get_user_model
 
 # 3rd party imports
 from rest_framework.mixins import CreateModelMixin
 from rest_framework.viewsets import GenericViewSet
 
 # project imports
-from djangorave.models import TransactionModel
+from djangorave.models import TransactionModel, PaymentMethodModel
 from djangorave.serializers import TransactionSerializer
 
 
 class TransactionApiView(CreateModelMixin, GenericViewSet):
-    """Provides ability create transactions"""
+    """Provides an api end point to create transactions"""
 
     queryset = TransactionModel.objects.all()
     serializer_class = TransactionSerializer
-    # permission_classes = (ConfigListRetrieveUpdatePermission,)
 
     def perform_create(self, serializer):
-        print("here" * 10)
-        serializer.save(user=self.request.user)
+        """Add payment_method and user to Transaction instance, determined
+        from the received reference"""
+        reference = serializer.validated_data["reference"]
+        payment_method_id = reference.split("__")[0]
+        user_id = reference.split("__")[2]
+        serializer.save(
+            user=get_user_model().objects.get(id=user_id),
+            payment_method=PaymentMethodModel.objects.get(id=payment_method_id),
+        )
